9d6a8c2c3ceb9899392ad5e839fd212c544dbf4d
The Peregrn Protocol <tsatoshiearn@gmail.com>
2025-07-06 08:12:24 +0200

    Update slowmo.js

--- js/slowmo.js
+++ js/slowmo.js
@@ -1,17 +1,1 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const btn = document.getElementById('slowMoBtn');
-  const statusText = document.getElementById('status');
-  const logo = document.querySelector('.logo');
 
-  let isActive = false;
-
-  function toggleSlowMo() {
-    isActive = !isActive;
-
-    logo.classList.toggle('slowmo', isActive);
-    statusText.textContent = isActive ? 'Effet SlowMo activé ✨' : 'Effet désactivé';
-    btn.textContent = isActive ? 'Désactiver le SlowMo' : 'Activer le SlowMo';
-  }
-
-  btn.addEventListener('click', toggleSlowMo);
-});
